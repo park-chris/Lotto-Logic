66d9d84d31d492ee778ae41ac424515c37735b0e
박수정 <zxzx4342@naver.com>
2021-12-26 10:23:34 +0900

    로또시뮬레이터화면 - 데이터바인딩 세팅

--- app/src/main/java/com/crystal/finalproject_logictest_211226/LottoSimulatorActivity.kt
+++ app/src/main/java/com/crystal/finalproject_logictest_211226/LottoSimulatorActivity.kt
@@ -2,10 +2,23 @@ package com.crystal.finalproject_logictest_211226
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.crystal.finalproject_logictest_211226.databinding.ActivityLottoSimulatorBinding
 
-class LottoSimulatorActivity : AppCompatActivity() {
+class LottoSimulatorActivity : BaseActivity() {
+
+    lateinit var binding : ActivityLottoSimulatorBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lotto_simulator)
+        binding= DataBindingUtil.setContentView(this, R.layout.activity_lotto_simulator)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
     }
 }
